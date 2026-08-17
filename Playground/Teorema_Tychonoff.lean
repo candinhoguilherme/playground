2df54cb2b7e41a248da670af1d9b8d9373f68050
Guilherme Candinho <candinhoguilherme@tuta.com>
2026-08-16 21:08:25 -0300

    more stuff
    Signed-off-by: Guilherme Candinho <candinhoguilherme@tuta.com>

--- Playground/Teorema_Tychonoff.lean
+++ Playground/Teorema_Tychonoff.lean
@@ -10,4 +10,40 @@ example : IsCompact (Set.Icc (0 : ℝ) 1 ×ˢ Set.Icc (0 : ℝ) 1) := by
 -- a topologia produto. (Ou seja, a topologia com respeito as projeções
 -- p_i: X → X_i ).
 
-variable {ι : Type*} (X : ι → Type*) [∀ i, TopologicalSpace (X i)]
+variable {ι : Type*} (X : ι → Type*)
+  [∀ i, TopologicalSpace (X i)]
+  [∀ i, CompactSpace (X i)]
+-- Cada conjunto X_i é um espaço topologico e é compacto.
+
+-- Definindo o espaço produto
+def ProductSpace := Π i, X i
+
+-- As projeções
+def proj (i : ι) : (Π j, X j) → X i := fun f => f i
+
+-- Trabalhando com bases e sub bases.
+
+-- A topologia gerada por S (menor topologia contendo S)
+def generateFrom {X : Type*} (S : Set (Set X)) : TopologicalSpace X :=
+  ⨆ (t : TopologicalSpace X) (_ : S ⊆ t.IsOpen), t
+
+-- S é uma subbase para a topologia se a gera
+def IsSubbasis {X : Type*} (S : Set (Set X)) [TopologicalSpace X] : Prop :=
+  generateFrom S = ‹TopologicalSpace X›
+
+-- Teorema de Alexander: X é compacto se, e somente se,
+-- toda cobertura por elementos da subbase S tem subcobertura finita
+theorem alexander_subbasis {X : Type*} [TopologicalSpace X] (S : Set (Set X))
+    (hS : IsSubbasis S) :
+    IsCompact (Set.univ : Set X) ↔
+    ∀ (C : Set (Set X)), C ⊆ S → (Set.univ ⊆ ⋃₀ C) →
+      ∃ D ⊆ C, D.Finite ∧ Set.univ ⊆ ⋃₀ D := by
+  sorry
+
+-- Define subbasis dos intervalos (-∞, a) e (b, ∞)
+def leftInterInfty (a : ℝ) : Set ℝ := {x | x < a}
+def rightInterInfty (b : ℝ) : Set ℝ := {x | b < x}
+
+-- Subbase para a topologia usual de ℝ
+def interSubbasis : Set (Set ℝ) :=
+  {U | ∃ a : ℝ, U = leftInterInfty a ∨ U = rightInterInfty a}
