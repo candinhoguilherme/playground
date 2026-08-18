68c494cc4573e34476ed9e50a0491d27d9bec202
Guilherme Candinho <candinhoguilherme@tuta.com>
2026-08-18 01:49:20 -0300

    update tychonoff e gcd added
    Signed-off-by: Guilherme Candinho <candinhoguilherme@tuta.com>

--- Playground/Teorema_Tychonoff.lean
+++ Playground/Teorema_Tychonoff.lean
@@ -15,6 +15,10 @@ variable {ι : Type*} (X : ι → Type*)
   [∀ i, CompactSpace (X i)]
 -- Cada conjunto X_i é um espaço topologico e é compacto.
 
+variable (Y : ℕ → Type*) -- com ι = ℕ, o conceito é o mesmo
+  [∀ i, TopologicalSpace (Y i)]
+  [∀ i, CompactSpace (Y i)]
+
 -- Definindo o espaço produto
 def ProductSpace := Π i, X i
 
@@ -24,6 +28,7 @@ def proj (i : ι) : (Π j, X j) → X i := fun f => f i
 -- Trabalhando com bases e sub bases.
 
 -- A topologia gerada por S (menor topologia contendo S)
+@[instance_reducible] -- Basicamente para evitar problemas com isDefEq.
 def generateFrom {X : Type*} (S : Set (Set X)) : TopologicalSpace X :=
   ⨆ (t : TopologicalSpace X) (_ : S ⊆ t.IsOpen), t
 
@@ -38,12 +43,4 @@ theorem alexander_subbasis {X : Type*} [TopologicalSpace X] (S : Set (Set X))
     IsCompact (Set.univ : Set X) ↔
     ∀ (C : Set (Set X)), C ⊆ S → (Set.univ ⊆ ⋃₀ C) →
       ∃ D ⊆ C, D.Finite ∧ Set.univ ⊆ ⋃₀ D := by
-  sorry
-
--- Define subbasis dos intervalos (-∞, a) e (b, ∞)
-def leftInterInfty (a : ℝ) : Set ℝ := {x | x < a}
-def rightInterInfty (b : ℝ) : Set ℝ := {x | b < x}
-
--- Subbase para a topologia usual de ℝ
-def interSubbasis : Set (Set ℝ) :=
-  {U | ∃ a : ℝ, U = leftInterInfty a ∨ U = rightInterInfty a}
+      sorry
