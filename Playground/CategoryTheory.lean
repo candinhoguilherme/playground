9633d01194d6f57431f458a04495bafbed2de680
Guilherme Candinho <candinhoguilherme@tuta.com>
2026-08-26 11:31:43 -0300

    better code for category theory after some study
    Signed-off-by: Guilherme Candinho <candinhoguilherme@tuta.com>

--- Playground/CategoryTheory.lean
+++ Playground/CategoryTheory.lean
@@ -1,18 +1,17 @@
--- Identidade
-def f (a : Int) := a + 1
-def my_id (a : Int) := a - 1
-#eval my_id (f 1) == 1 -- Verifica se my_id funciona
--- Composição
-def g (a : Int) := a * 2
-def g ∘ f := g f a
-#eval g ∘ f 1
+def identity {α : Type} (x : α) : α :=
+  x
 
--- Desafios págia 43 / 25
+def compose {α β γ : Type} (f : β → γ) (g : α → β) : α → γ :=
+  fun x => f (g x)
 
--- Sim, a WWW é uma categoria que relaciona páginas (objetos) a outras páginas (hyperlinks)
--- Sim, hyperlinks são morfismos
+theorem compose_identity_left
+    {α β : Type} (f : α → β) :
+    compose identity f = f := by
+  funext x
+  rfl
 
--- Sim, um morfismo de uma pessoa nela mesma séria a identidade e as relações de amizades
--- levam uma pessoa a um amigo em particular.
-
--- Pesquisar "Quando directed graph é uma categoria"
+theorem compose_identity_right
+    {α β : Type} (f : α → β) :
+    compose f identity = f := by
+  funext x
+  rfl
