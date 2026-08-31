c6af74f6289352038775d511b3c516af1a80f821
Guilherme Candinho <candinhoguilherme@tuta.com>
2026-08-31 18:00:19 -0300

    updated some files and deleted unused one's
    Signed-off-by: Guilherme Candinho <candinhoguilherme@tuta.com>

--- Playground.lean
+++ Playground.lean
@@ -1,1 +1,10 @@
 import Playground.Basic
+
+def main (args : List String) : IO Unit := do
+  let argc := args.length
+
+  if argc < 2 then
+    IO.println "{\"authorized\":false,\"error\":\"No JSON received\"}"
+  else
+    let json := getElem! args 1
+    IO.println json
