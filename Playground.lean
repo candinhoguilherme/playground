43668f3c9811344abc646237294e03b63222e9de
Guilherme Candinho <candinhoguilherme@tuta.com>
2026-09-01 01:52:42 -0300

    update basic
    Signed-off-by: Guilherme Candinho <candinhoguilherme@tuta.com>

--- Playground.lean
+++ Playground.lean
@@ -1,10 +1,2 @@
-import Playground.Basic
-
-def main (args : List String) : IO Unit := do
-  let argc := args.length
-
-  if argc < 2 then
-    IO.println "{\"authorized\":false,\"error\":\"No JSON received\"}"
-  else
-    let json := getElem! args 1
-    IO.println json
+def main : IO Unit := do
+    IO.println "Hello World!"
